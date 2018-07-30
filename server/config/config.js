4de8110ceb6853cd46f113e53162ffb6cf93795a
Alejandro <apricuatro@hotmail.com>
2018-07-29 22:12:08 -0400

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:a123456@ds137687.mlab.com:37687/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.dicmongo = urlDB;
